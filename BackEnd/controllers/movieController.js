63922ee48615318a143eecb07a9fab4bfe4e0b96
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2024-12-19 13:19:19 +0530

    Updated code

--- BackEnd/controllers/movieController.js
+++ BackEnd/controllers/movieController.js
@@ -1,151 +1,6 @@
-// const { v4: uuidv4 } = require("uuid");
-
-// const MovieModel = require("../model/movieModel");
-
-// //create Movie
-// exports.createMovie = async (req, res) => {
-//   const {
-//     name,
-//     description,
-//     genre,
-//     cast,
-//    director,
-//     producer,
-//     rating,
-//     releaseDate,
-//   } = req.body;
-
-//   console.log(req.body);
-//   if (
-//     !name ||
-//     !description ||
-//     !genre ||
-//     !cast ||
-//     !director ||
-//     !producer ||
-//     !releaseDate
-//   ) {
-//     return res.status(400).json({ message: "Missing required Fields." });
-//   }
-
-//   const movieId = `MOVIES#${uuidv4()}`;
-//   const movieData = {
-//     PK: movieId,
-//     SK: "METADATA",
-//     Entity: "MOVIE",
-//     MovieName: name,
-//     Description: description,
-//     Genre: genre,
-//     Cast: cast,
-//     Director: director,
-//     Producer: producer,
-//     Rating: rating,
-//     ReleaseDate: releaseDate,
-//   };
-
-//   try {
-//     await MovieModel.createMovie(movieData);
-//     return res
-//       .status(201)
-//       .json({ movieId, message: "Movie Created Successfully" });
-//   } catch (err) {
-//     console.log("Error in creating movie ", err);
-//     return res
-//       .status(400)
-//       .json({ movieId, message: "Error in creating Movie." });
-//   }
-// };
-
-// //fetch movie details by name
-// exports.getMovieDetails = async (req, res) => {
-//   const { movieName } = req.params;
-//   // console.log(movieName,'++++++++++')
-//   if (!movieName) {
-//     return res.status(400).json({ message: "Movie name is not provided." });
-//   }
-//   try {
-//     const data = await MovieModel.getMovieByName(movieName);
-//     // console.log('--======', data.Items);
-//     if (!data.Items || data.Items.length === 0) {
-//       return res.status(404).json({ message: "Movie Not Found" });
-//     } else {
-//       const response = data.Items[0];
-//       // return res.status(200).json({movie, message : "Movie found successfully."})
-//       return res.status(200).json({
-//           response,
-//         message: "Movie Found",
-//       });
-//     }
-//   } catch (err) {
-//     console.log("Error in fetching movie ", err);
-//     return res.status(500).json({ message: "Error fetching movie details" });
-//   }
-// };
-
-// //fetcch all movies by releaseDate
-// exports.getSortedMovies = async (req, res) => {
-//   // const movieId = req.params
-//   const ReleaseDate = req.query.releaseDate;
-
-//   // console.log(ReleaseDate,'---=====');
-//   if (!ReleaseDate) {
-//     try {
-//         const data = await MovieModel.getAllMovies();
-//         // let response;
-//         if (data && data.Items && data.Items.length > 0) {
-//           // console.log(data.Items);
-//           const response = data.Items;
-//           return res.status(200).json({ response, message: "Movie Found" });
-//         } else {
-//           return res.status(404).json({ message: "No Movie exist." });
-//         }
-//         // else return data;
-//       } catch (err) {
-//         console.log("Error fetching all movies", err);
-//         return res.status(500).json({ message: "Error Fetching Movies" });
-//       }
-//     // return res.status(400).json({ message: "Release date is required" });
-//   }
-
-//   try {
-//     const data = await MovieModel.getAllMoviesByDate(ReleaseDate);
-//     // let response;
-//     if (data && data.Items && data.Items.length > 0) {
-//       // console.log(data.Items);
-//       const response = data.Items;
-//       return res.status(200).json({ response, message: "Movie Found" });
-//     } else {
-//       return res.status(404).json({ message: "No Movie exist." });
-//     }
-//     // else return data;
-//   } catch (err) {
-//     console.log("Error fetching all movies", err);
-//     return res.status(500).json({ message: "Error Fetching Movies" });
-//   }
-// };
-
-// // filter movies by genre
-// exports.filterByGenre = async (req, res) => {
-//   const { genre } = req.query; //using {} this becasue getting genre as a object so destructuring it.
-
-//   if (!genre) {
-//     return res.status(400).json({ message: "Genre not provided." });
-//   }
-//   try {
-//     // console.log('455455')
-//     const data = await MovieModel.filterMoviesByGenre(genre);
-//     return res.status(200).json(data.Items);
-//   } catch (err) {
-//     console.log("Error in fetching movie by genre", err);
-//     return res
-//       .status(500)
-//       .json({ message: "Error in fetching movie by genre" });
-//   }
-// };
-
 const { v4: uuidv4 } = require("uuid");
 const MovieModel = require("../model/movieModel");
-const { redisCacheCall } = require("../utils/redisCalls"); 
+const { redisCacheCall } = require("../utils/redisCalls");
 
 // Cache expiry time in seconds (e.g., 1 hour)
 const CACHE_EXPIRY = 3600;
@@ -153,10 +8,26 @@ const CACHE_EXPIRY = 3600;
 // Create Movie
 exports.createMovie = async (req, res) => {
   const {
-    name,    description,    genre,    cast,    director,    producer,    rating,    releaseDate,  } = req.body;
+    name,
+    description,
+    genre,
+    cast,
+    director,
+    producer,
+    rating,
+    releaseDate,
+  } = req.body;
 
-// console.log("Req body is..." , req.body);
-  if (!name || !description || !genre || !cast || !director || !producer || !releaseDate) {
+  // console.log("Req body is..." , req.body);
+  if (
+    !name ||
+    !description ||
+    !genre ||
+    !cast ||
+    !director ||
+    !producer ||
+    !releaseDate
+  ) {
     return res.status(400).json({ message: "Missing required fields." });
   }
 
@@ -240,9 +111,7 @@ exports.getSortedMovies = async (req, res) => {
   const ReleaseDate = req.query.releaseDate;
 
   try {
-    const cacheKey = ReleaseDate
-      ? `Movie:Sorted:${ReleaseDate}`
-      : "ALL_MOVIES";
+    const cacheKey = ReleaseDate ? `Movie:Sorted:${ReleaseDate}` : "ALL_MOVIES";
 
     // Check Redis cache first
     const cachedData = await redisCacheCall("get", cacheKey);
--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -1,251 +1,3 @@
-// const UserModel = require("../model/userModel");
-// const BookingModel = require("../model/bookingModel");
-// const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
-
-// //Signup user
-// exports.signupUser = async (req, res) => {
-//   // console.log(req.body , 'req body');
-//   let { name, email, phone, password, role } = req.body;
-//   console.log(name , email , phone, password);
-//   if (!name || !email || !phone || !password) {
-//     return res.status(400).json({ message: "Missing required fields." });
-//   }
-
-//   if (role !== "Admin") {
-//     role = "notAdmin";
-//   }
-
-//   const userId = `USER#${email}`; //creating user Id
-
-//   //hashing password
-//   const saltRounds = 10;
-//   hashedPassword = await bcrypt.hash(password, saltRounds);
-//   const userData = {
-//     PK: userId,
-//     SK: "METADATA",
-//     Entity: "USER",
-//     UserId: userId,
-//     Name: name,
-//     Email: email,
-//     Phone: phone,
-//     Password: hashedPassword,
-//     Bookings: [],
-//     Role: role,
-//   };
-
-//   try {
-//     // console.log('inside controller -------',userData);
-//     await UserModel.createUser(userData);
-//     // console.log('---------results ',result);
-//     return res
-//       .status(201)
-//       .json({ userId, message: "User Created Successfully" });
-//   } catch (error) {
-//     if (error.code === "ConditionalCheckFailedException") {
-//       return res.status(409).json({
-//         success: false,
-//         message: "User already exists.",
-//       });
-//     }
-//     // Handle other errors
-//     console.error("Error querying DynamoDB (createUser):", error);
-//     return res.status(500).json({
-//       success: false,
-//       message: "An error occurred while creating the user.",
-//       errorDetails: error.message,
-//     });
-//   }
-// };
-
-// //login User
-
-// exports.loginUser = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-
-//     // Validate input
-//     if (!email || !password) {
-//       return res
-//         .status(400)
-//         .json({ message: "Email and password are both required." });
-//     }
-
-//     // Fetch user by email
-//     const userResponse = await UserModel.loginUser(email);
-
-//     // Handle user response
-//     if (!userResponse.success) {
-//       return res.status(userResponse.statusCode).json({
-//         message: userResponse.message,
-//       });
-//     }
-
-//     const user = userResponse.data;
-
-//     // Compare passwords
-//     const isPasswordValid = await bcrypt.compare(password, user.Password);
-//     if (!isPasswordValid) {
-//       return res.status(401).json({ message: "Invalid credentials." });
-//     }
-
-//     // Generate JWT
-//     const token = jwt.sign(
-//       {
-//         id: user.UserId,
-//         email: user.Email,
-//         role: user.Role,
-//       },
-//       process.env.JWT_SECRET,
-//       { expiresIn: "24h" }
-//     );
-
-//     // Return success response with token
-//     return res.status(200).json({
-//       message: "Login successful.",
-//       userId: user.UserId,
-//       name: user.Name,
-//       email: user.Email,
-//       token: `Bearer ${token}`, // Standard Bearer token format
-//     });
-//   } catch (error) {
-//     console.error("Error logging in user:", error);
-//     return res.status(500).json({
-//       message: "Internal server error. Unable to log in.",
-//     });
-//   }
-// };
-
-// //fetch user
-// exports.fetchUser = async (req, res) => {
-//   const userId = "USER#" + req.params.userId; //since # in userid will create problems, therefore just asing user emai in id and adding "user#"
-//   // console.log('++----' , userId);
-//   const JwtUserId = req.user.id; //extracting userId from token to verify with the userId passed in the params.
-
-//   if (userId !== JwtUserId) {
-//     return res.status(409).json({ message: "Different UserId is used." });
-//   }
-//   try {
-//     const data = await UserModel.getUser(userId);
-//     // console.log(data,'---------------')
-//     if (!data) return res.status(404).json({ message: "User not found" });
-//     return res.status(200).json(data);
-//   } catch (err) {
-//     console.log("Error in fetching user", err);
-//     return res.status(500).json({ message: "Error fetching user" });
-//   }
-// };
-
-// //update User
-// exports.updateUser = async (req, res) => {
-//   const userId = "USER#" + req.params.userId;
-//   const { name, phone, password } = req.body;
-
-//   if (!name && !phone && !password) {
-//     return res.status(406).json({ message: "Empty Request." });
-//   }
-
-//   const JwtUserId = req.user.id; //extracting userId from token to verify with the userId passed in the params.
-
-//   if (userId !== JwtUserId) {
-//     return res.status(403).json({ message: "Different UserId is used." });
-//   }
-//   const updateExpression = [];
-//   const expressionAttributeValues = {};
-//   const expressionAttributeNames = {};
-//   if (name) {
-//     // updateExpression.push("Name = :name");
-//     updateExpression.push("#name = :name");
-//     expressionAttributeValues[":name"] = name;
-//     expressionAttributeNames["#name"] = "name";
-//   }
-//   if (phone) {
-//     updateExpression.push("Phone = :phone");
-//     expressionAttributeValues[":phone"] = phone;
-//   }
-//   if (password) {
-//     const hashedPassword = await bcrypt.hash(password, 10);
-//     updateExpression.push("Password = :password");
-//     expressionAttributeValues[":password"] = hashedPassword;
-//   }
-
-//   try {
-//     await UserModel.updateUser(
-//       userId,
-//       `SET ${updateExpression.join(", ")}`,
-//       expressionAttributeValues,
-//       expressionAttributeNames
-//     );
-//     return res.status(200).json({ message: "User Updated Successfully" });
-//   } catch (err) {
-//     console.log("Error in updating user ", err);
-//     return res.status(500).json({ message: "Error updating user" });
-//   }
-// };
-
-// //delete User
-// exports.deleteUser = async (req, res) => {
-//   // const userId = req.params.userId;
-
-//   let { userId } = req.params;
-//   userId = "USER#" + userId;
-
-//   // const status = 'confirmed';
-
-//   const currentTime = Math.floor(Date.now() / 1000); //converting time to epoch time
-
-//   const JwtUserId = req.user.id; //extracting userId from token to verify with the userId passed in the params.
-
-//   if (userId !== JwtUserId) {
-//     return res.status(409).json({ message: "Different UserId is used." });
-//   }
-
-//   try {
-//     // console.log('curre' , currentTime.toString());
-//     const bookings = await BookingModel.getUpcomingBookings(
-//       userId,
-//       currentTime.toString()
-//     );
-//     // console.log('-=-=-=-=-',bookings , userId);
-//     if (bookings.length > 0) {
-//       return res
-//         .status(404)
-//         .json({ message: "You have upcoming bookings, can not delte account" });
-//     }
-//   } catch (err) {
-//     console.error(err);
-//     return res.status(500).json({ message: "Error fetching bookings by time" });
-//   }
-
-//   try {
-//     // Fetch User Details to get bookings
-
-//     const userData = await UserModel.getUser(userId);
-//     // console.log(userData, '---323232----');
-//     if (!userData) {
-//       return res.status(404).json({ message: "User not found." });
-//     }
-
-//     const bookings = userData.bookings || [];
-
-//     // Delete All Bookings
-//     bookings.map(async (bookingId) => {
-//       await UserModel.deleteAllBookings(userId, bookingId);
-//     });
-//     // await Promise.all(bookings.map((bookingId) => UserModel.deleteAllBookings(userId, bookingId)));
-
-//     console.log("trying to delete user: ");
-//     //Delete user
-//     await UserModel.deleteUser(userId);
-//     return res.status(200).json({ message: "User deleted successfully" });
-//   } catch (err) {
-//     console.log("Error in deleting user ", err);
-//     return res.status(500).json({ message: "Error in deleting user" });
-//   }
-// };
-
-
-
 const UserModel = require("../model/userModel");
 const BookingModel = require("../model/bookingModel");
 const bcrypt = require("bcrypt");
@@ -286,7 +38,9 @@ exports.signupUser = async (req, res) => {
     await UserModel.createUser(userData);
     // Invalidate Redis cache for users
     await redisCacheCall("del", userId);
-    return res.status(201).json({ userId, message: "User Created Successfully" });
+    return res
+      .status(201)
+      .json({ userId, message: "User Created Successfully" });
   } catch (error) {
     if (error.code === "ConditionalCheckFailedException") {
       return res.status(409).json({
@@ -309,7 +63,9 @@ exports.loginUser = async (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
-      return res.status(400).json({ message: "Email and password are both required." });
+      return res
+        .status(400)
+        .json({ message: "Email and password are both required." });
     }
 
     // Check Redis cache first
@@ -318,7 +74,9 @@ exports.loginUser = async (req, res) => {
       // Fetch user by email from DB if not cached
       const userResponse = await UserModel.loginUser(email);
       if (!userResponse.success) {
-        return res.status(userResponse.statusCode).json({ message: userResponse.message });
+        return res
+          .status(userResponse.statusCode)
+          .json({ message: userResponse.message });
       }
       user = userResponse.data;
 
@@ -350,7 +108,9 @@ exports.loginUser = async (req, res) => {
     });
   } catch (error) {
     console.error("Error logging in user:", error);
-    return res.status(500).json({ message: "Internal server error. Unable to log in." });
+    return res
+      .status(500)
+      .json({ message: "Internal server error. Unable to log in." });
   }
 };
 
